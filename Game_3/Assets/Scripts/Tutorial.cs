4f7aaf090f67696aab339fd1c0674f71f157597f
Tim <25timking25@gmail.com>
2024-12-02 20:56:47 -0500

    Minor 'bug' resolved in tutorial script

--- Game_3/Assets/Scripts/Tutorial.cs
+++ Game_3/Assets/Scripts/Tutorial.cs
@@ -8,6 +8,7 @@ public class Tutorial : MonoBehaviour
 {
     public TextMeshProUGUI tutorialText;
     private bool isWaitingForInput = false;
+    private bool waitForSkip = false;
 
     // Start is called before the first frame update
     void Start()
@@ -21,6 +22,7 @@ public class Tutorial : MonoBehaviour
     {
         // Enables the user to manually skip the tutorial
         if (Input.GetKeyDown(KeyCode.Backspace)) {
+            waitForSkip = true;
             StartCoroutine(endTutorial());
         }
     }
@@ -29,7 +31,7 @@ public class Tutorial : MonoBehaviour
     {
         Debug.Log("'RunTutorial' Ran");
         tutorialText.text = "Press 'Backspace' at any time to skip tutorial";
-        //yield return StartCoroutine(DestroyMessageAfterDelay(5f)); // Wait for initial message to disappear
+        yield return StartCoroutine(DestroyMessageAfterDelay(5f)); // Wait for initial message to disappear
 
         yield return StartCoroutine(learnMovement());
         yield return StartCoroutine(learnEmotes());
@@ -42,38 +44,45 @@ public class Tutorial : MonoBehaviour
     // Destroys intital instructions after a specified delay
     private IEnumerator DestroyMessageAfterDelay(float delay) {
         yield return new WaitForSeconds(delay);
-        Destroy(tutorialText.gameObject);
     }
 
     // Instructs the user on basic movement controls
     IEnumerator learnMovement() {
-        Debug.Log("'learnMovement' Ran");
-        tutorialText.text = "Use WASD to move forwards, left, backwards, and right. You can use 'Space' to jump.  ('Enter' to continue)";
-        yield return new WaitUntil(() => Input.GetKeyDown(KeyCode.Return));
+        if (waitForSkip != true) {
+            Debug.Log("'learnMovement' Ran");
+            tutorialText.text = "Use WASD to move forwards, left, backwards, and right. You can use 'Space' to jump.  ('Enter' to continue)";
+            yield return new WaitUntil(() => Input.GetKeyDown(KeyCode.Return));
+        }
     }
 
     // Instructs the user on emote controls
     IEnumerator learnEmotes() {
         yield return new WaitUntil(() => !Input.GetKeyDown(KeyCode.Return));
-        Debug.Log("'learnEmotes' Ran");
-        tutorialText.text = "Use 1 - 6 to activate emotes. Press 7 to stop.  ('Enter' to continue)";
-        yield return new WaitUntil(() => Input.GetKeyDown(KeyCode.Return));
+        if (waitForSkip != true) {
+            Debug.Log("'learnEmotes' Ran");
+            tutorialText.text = "Use 1 - 6 to activate emotes. Press 7 to stop.  ('Enter' to continue)";
+            yield return new WaitUntil(() => Input.GetKeyDown(KeyCode.Return));
+        }
     }
 
     // Instructs the user on basic portal controls
     IEnumerator learnPortals() {
         yield return new WaitUntil(() => !Input.GetKeyDown(KeyCode.Return));
-        Debug.Log("'learnPortals' Ran");
-        tutorialText.text = "When you get access to portals, use 'left click' and 'right click' to shoot portals. ('Enter' to continue)";
-        yield return new WaitUntil(() => Input.GetKeyDown(KeyCode.Return));
+        if (waitForSkip != true) {
+            Debug.Log("'learnPortals' Ran");
+            tutorialText.text = "When you get access to portals, use 'left click' and 'right click' to shoot portals. ('Enter' to continue)";
+            yield return new WaitUntil(() => Input.GetKeyDown(KeyCode.Return));
+        }
     }
 
     // Provides overall game objective / narrative
     IEnumerator learnObjective() {
-                yield return new WaitUntil(() => !Input.GetKeyDown(KeyCode.Return));
-        Debug.Log("'learnObjective' Ran");
-        tutorialText.text = "Each level is a puzzle to solve. Use your portals to move to the next stage! ('Enter' to continue)";
-        yield return new WaitUntil(() => Input.GetKeyDown(KeyCode.Return));
+        yield return new WaitUntil(() => !Input.GetKeyDown(KeyCode.Return));
+        if (waitForSkip != true) {
+            Debug.Log("'learnObjective' Ran");
+            tutorialText.text = "Each level is a puzzle to solve. Use your portals to move to the next stage! ('Enter' to continue)";
+            yield return new WaitUntil(() => Input.GetKeyDown(KeyCode.Return));
+        }
     }
 
     // Changes scene from the tutorial to the actual game
