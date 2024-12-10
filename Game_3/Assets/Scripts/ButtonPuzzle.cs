46a67c7571b6884bd2bbdf25968dd2331a85c6f0
john-hershey <jjhersh1@millersville.edu>
2024-12-10 16:48:02 -0500

    level 2 intro text

--- Game_3/Assets/Scripts/ButtonPuzzle.cs
+++ Game_3/Assets/Scripts/ButtonPuzzle.cs
@@ -5,12 +5,16 @@ using UnityEngine.UI;
 
 public class ButtonPuzzle : MonoBehaviour
 {
-    public Text box;
     private string doorText = "A door has opened";
+    private float minDispTime = 2f;
+    public GameObject light;
+    public Text box;
+    
+
     // Start is called before the first frame update
     void Start()
     {
-        box.text = "Welcome to the Cube test";
+        StartCoroutine(DisplayText("Welcome to the Cube Test", "Use the cubes to press a series of buttons to open doors to escape"));
     }
 
     // Update is called once per frame
@@ -18,5 +22,12 @@ public class ButtonPuzzle : MonoBehaviour
     {
         
     }
+    IEnumerator DisplayText(string message, string next)
+    {
+        box.text = message;
+        yield return new WaitForSeconds(minDispTime + message.length * 0.1f);
+        StartCoroutine(DisplayText(next, ""));
+    }
+
     
 }
